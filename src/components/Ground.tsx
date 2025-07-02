f7822972865e44bd193ad4e5007c39f9a1488c93
andsaki <ruby.on.rails.api@gmail.com>
2025-07-02 14:40:16 +0900

    Update project files including sundial orientation and other modifications

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -4,7 +4,11 @@ const Ground: React.FC = () => {
   return (
     <group>
       {/* Ground plane to simulate the bottom of the pond, extended much further, positioned at slightly negative Y */}
-      <mesh position={[0, -1, -2]} rotation={[-Math.PI / 2, 0, 0]}>
+      <mesh
+        position={[0, -1, -2]}
+        rotation={[-Math.PI / 2, 0, 0]}
+        receiveShadow={true}
+      >
         <planeGeometry args={[80, 80, 4, 4]} />{" "}
         {/* Reduced subdivisions for better performance */}
         <meshStandardMaterial
--- src/components/PottedPlant.tsx
+++ src/components/PottedPlant.tsx
@@ -4,33 +4,41 @@ import { useGLTF } from "@react-three/drei";
 const PottedPlant: React.FC = () => {
   return (
     <group>
-      {/* Potted Plant 1 - positioned on the ground */}
+      {/* Potted Plant 1 - positioned further away on the ground */}
       <primitive
         object={useGLTF("/assets/Potted Plant 1/scene.gltf").scene}
-        position={[-4, -1, -3]}
+        position={[-6, -1, -5]}
         rotation={[0, 0, 0]}
         scale={[0.3, 0.3, 0.3]}
+        castShadow={true}
+        receiveShadow={true}
       />
-      {/* Potted Plant 2 - positioned on the ground */}
+      {/* Potted Plant 2 - positioned further away on the ground */}
       <primitive
         object={useGLTF("/assets/Potted Plant 1/scene.gltf").scene}
-        position={[4, -1, -2]}
+        position={[6, -1, -4]}
         rotation={[0, Math.PI / 4, 0]}
         scale={[0.25, 0.25, 0.25]}
+        castShadow={true}
+        receiveShadow={true}
       />
-      {/* Potted Plant 3 - positioned on the ground */}
+      {/* Potted Plant 3 - positioned further away on the ground */}
       <primitive
         object={useGLTF("/assets/Potted Plant 1/scene.gltf").scene}
-        position={[-3, -1, 3]}
+        position={[-5, -1, 5]}
         rotation={[0, Math.PI / 2, 0]}
         scale={[0.35, 0.35, 0.35]}
+        castShadow={true}
+        receiveShadow={true}
       />
-      {/* Potted Plant 4 - positioned on the ground */}
+      {/* Potted Plant 4 - positioned further away on the ground */}
       <primitive
         object={useGLTF("/assets/Potted Plant 1/scene.gltf").scene}
-        position={[5, -1, -4]}
+        position={[7, -1, -6]}
         rotation={[0, -Math.PI / 4, 0]}
         scale={[0.2, 0.2, 0.2]}
+        castShadow={true}
+        receiveShadow={true}
       />
     </group>
   );
