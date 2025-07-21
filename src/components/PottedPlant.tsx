e791f13919454f4cab4dc89066b5401047b0861d
andsaki <ruby.on.rails.api@gmail.com>
2025-07-21 13:52:47 +0900

    fix: パス名

--- src/components/PottedPlant.tsx
+++ src/components/PottedPlant.tsx
@@ -29,8 +29,8 @@ const PottedPlant: React.FC = () => {
   // ローカルとCloudflare Workerのどちらを参照するかを環境変数で切り替え
   const isLocal = import.meta.env.VITE_ENVIRONMENT === "local";
   const modelUrl = isLocal
-    ? "/assets/Potted Plant 1/scene.gltf"
-    : "https://biotope-r2-worker.ruby-on-rails-api.workers.dev/assets/Potted Plant 1/scene.gltf";
+    ? "/assets/Potted Plant/scene.gltf"
+    : "https://biotope-r2-worker.ruby-on-rails-api.workers.dev/assets/Potted Plant/scene.gltf";
   const { scene: plantScene1 } = modelUrl
     ? useGLTF(modelUrl, true)
     : { scene: new THREE.Group() };
